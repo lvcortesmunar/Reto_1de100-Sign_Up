9c967f95a1c254a971609b0305908c9c3812ac5c
LVCM <Lv.cortesmunar@gmail.com>
2026-06-18 19:30:53 -0500

    Explicación de los nuevos tecnicismos Importar (Import):
    ¿Qué es?: La línea import SignUpForm from ... en App.jsx.
    
    ¿Por qué?: Porque App.jsx y SignUpForm.jsx son archivos distintos.
    
    ¿Para qué?: Para traer (traer o incluir) el componente que creamos en una carpeta y poder usarlo dentro de otro archivo principal.
    
    Formulario (Form):
    
    ¿Qué es?: La etiqueta <form> en tu código.
    
    ¿Por qué?: Es la forma estándar en la web para capturar datos del usuario.
    
    ¿Para qué?: Para que, en el futuro, cuando el usuario escriba su información y presione "Enviar", podamos capturar esos datos (procesar la información) para crear su cuenta.

---
+++ src/features/auth/components/SignUpForm.jsx
@@ -0,0 +1,11 @@
+function SignUpForm() {
+  return (
+    <form>
+      <h2>Registro</h2>
+      <input type="text" placeholder="Nombre" />
+      <button type="submit">Enviar</button>
+    </form>
+  )
+}
+
+export default SignUpForm
